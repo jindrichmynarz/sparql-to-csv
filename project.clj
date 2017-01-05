8f3f2bbaf86225f936c1fe84d0fd7c2f603b8049
Jindrich Mynarz <mynarzjindrich@gmail.com>
2017-01-05 13:05:43 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sparql-to-csv "0.1.0"
+(defproject sparql-to-csv "0.2.0"
   :description "Stream SPARQL results to CSV"
   :url "http://github.com/jindrichmynarz/sparql-to-csv"
   :license {:name "Eclipse Public License"
